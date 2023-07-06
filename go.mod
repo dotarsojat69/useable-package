6f3aa5bb28dfe5a1fdfae6458ea454fa507cc381
dotarsojat69 <tdeath457@gmail.com>
2023-07-06 11:20:49 +0700

    Mengembalikan Nilai Matematika

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dotarsojat69/useable-package
+
+go 1.20
---
+++ usable.go
@@ -0,0 +1,36 @@
+package usable
+
+import (
+	"fmt"
+	"math"
+)
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func IsPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PrintHello(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
